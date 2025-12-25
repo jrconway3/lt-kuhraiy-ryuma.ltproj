acc83f46b95d9a3296693ce88388ffa87c2f5c07
JaidynReiman <jaidyn@jaidynreiman.net>
2025-12-24 23:36:51 -0600

    * Added Debug Issues, Adjusted Growths on Clergy/Bishop

--- resources/custom_components/custom_item_components.py
+++ resources/custom_components/custom_item_components.py
@@ -9,6 +9,9 @@ from app.engine.game_state import game
 from app.engine.objects.unit import UnitObject
 from app.utilities import utils, static_random
 
+from app.engine.fonts import NORMAL_FONT_COLORS, FONT
+from app.engine.game_menus.icon_options import UsesDisplayConfig
+
 
 class DoNothing(ItemComponent):
     nid = 'do_nothing'
@@ -18,35 +21,21 @@ class DoNothing(ItemComponent):
     expose = ComponentType.Int
     value = 1
 
-class ManaOptions(ItemComponent):
-    nid = 'mana_options'
-    desc = "Additional options for MANA uses."
-    paired_with = ('mana',)
+class CustomUsesColor(ItemComponent):
+    nid = 'custom_uses_color'
+    desc = "Override the default color of the uses text. Do not Combine with ManaCostAsUses or RemainingManaUses."
     tag = ItemTags.USES
+    value = 'white'
 
-    expose = ComponentType.NewMultipleOptions
+    expose = (ComponentType.MultipleChoice, ['white', 'blue', 'green', 'red', 'orange', 'grey', 'yellow', 'brown', 'purple', 'navy'])
 
-    options = {
-        'lose_mana_on_miss': ComponentType.Bool,
-        'one_loss_per_combat': ComponentType.Bool
-    }
+    def _font_color(self, unit, item):
+        color = self.value
+        if not item_funcs.available(unit, item):
+            color = 'grey'
+        if 'text-' + color in FONT:
+            return color
+        return None
 
-    def __init__(self, value=None):
-        self.value = {
-            'lose_mana_on_miss': True,
-            'one_loss_per_combat': False
-        }
-        if value and isinstance(value, dict):
-            self.value.update(value)
-        else: # value is a list from the old multiple options
-            try:
-                self.value['lose_mana_on_miss'] = value[0][1] == 'T'
-                self.value['one_loss_per_combat'] = value[1][1] == 'T'
-            except:
-                pass
-
-    def lose_mana_on_miss(self) -> bool:
-        return self.value.get('lose_mana_on_miss', False)
-
-    def one_loss_per_combat(self) -> bool:
-        return self.value.get('one_loss_per_combat', False)
+    def item_uses_display(self, unit, item) -> UsesDisplayConfig:
+        return UsesDisplayConfig(None, None, None, self._font_color, unit, item)
