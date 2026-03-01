94b416bfa4e97d26c710a780f103bc6d693d1edb
JaidynReiman <jaidyn@jaidynreiman.net>
2026-03-01 13:31:43 -0600

    * Various Refactors

--- resources/custom_components/custom_item_components.py
+++ resources/custom_components/custom_item_components.py
@@ -9,9 +9,6 @@ from app.engine.game_state import game
 from app.engine.objects.unit import UnitObject
 from app.utilities import utils, static_random
 
-from app.engine.fonts import NORMAL_FONT_COLORS, FONT
-from app.engine.game_menus.icon_options import UsesDisplayConfig
-
 
 class DoNothing(ItemComponent):
     nid = 'do_nothing'
@@ -20,22 +17,3 @@ class DoNothing(ItemComponent):
 
     expose = ComponentType.Int
     value = 1
-
-class CustomUsesColor(ItemComponent):
-    nid = 'custom_uses_color'
-    desc = "Override the default color of the uses text. Do not Combine with ManaCostAsUses or RemainingManaUses."
-    tag = ItemTags.USES
-    value = 'white'
-
-    expose = (ComponentType.MultipleChoice, ['white', 'blue', 'green', 'red', 'orange', 'grey', 'yellow', 'brown', 'purple', 'navy'])
-
-    def _font_color(self, unit, item):
-        color = self.value
-        if not item_funcs.available(unit, item):
-            color = 'grey'
-        if 'text-' + color in FONT:
-            return color
-        return None
-
-    def item_uses_display(self, unit, item) -> UsesDisplayConfig:
-        return UsesDisplayConfig(None, None, None, self._font_color, unit, item)
